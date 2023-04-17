2b48fd2f3e25b48977c10c814c73b99eec5789eb
yuhaofeng <2087953253@qq.com>
2023-04-17 19:25:18 +0800

    调整部署脚本2

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
-cp -r ./dist/index.html /app/
-cp -r ./dist/static/js /app/static/js
-cp -r ./dist/static/css /app/static/css
-cp -r ./dist/static/fonts /app/static/fonts
+cp -r ./dist/ /app/
 cp -r ./nginx.conf /usr/local/nginx/conf/nginx.conf
 cp -r ./default.conf /usr/local/nginx/conf.d/default.conf
 /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf;
